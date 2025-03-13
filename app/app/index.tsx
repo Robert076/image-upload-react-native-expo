6406386624db664090ea5bd2b48372a9ce8184fb
Robert076 <robertberes06@gmail.com>
2025-03-13 13:33:40 +0200

    feat: done

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -3,7 +3,29 @@ import { useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 
 const index = () => {
-  return <View></View>;
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"], // you can also do ['images', 'videos']
+      allowsEditing: false, // up to you
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // console.log(result)
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+  );
 };
 
 export default index;
